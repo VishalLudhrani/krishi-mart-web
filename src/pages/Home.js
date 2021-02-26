b0461adaeffae61f1578ffaf6f6698b68059fdb6
Vishal Ludhrani <vludrani@gmail.com>
2021-02-26 16:11:44 +0530

    added the splash screen feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,8 @@ class Home extends React.Component {
     filterValue: 4,
     queryNull: 'display-block row',
     searchQuery: null,
-    nullMessage: null
+    nullMessage: null,
+    isLoading: true
   }
 
   componentDidMount() {
@@ -21,14 +22,19 @@ class Home extends React.Component {
     firebase.auth().onAuthStateChanged((user) =>{
       if (user) {
         // User is signed in.
-        this.setState({loggedInUser: user.displayName});
-        // change the style for login element
-        this.setState({loginStyle: "display-none"});
-        this.setState({logoutStyle: "display-block row"});
+        this.setState({loggedInUser: user.displayName,
+        // change the style for login element,
+        loginStyle: "display-none",
+        logoutStyle: "display-block row",
+        isLoading: false
+      });
       } else { // user is signed out
         // display the webpage for the user to register/login
-        this.setState({loginStyle: "display-block row heroSection"});
-        this.setState({logoutStyle: "display-none"});
+        this.setState({
+          loginStyle: "display-block row heroSection",
+          logoutStyle: "display-none",
+          isLoading: false
+        });
       }
     });
     if(this.state.products.length === 0) {
@@ -42,48 +48,61 @@ class Home extends React.Component {
   }
 
   render() {
-    return(
-      <div>
-        <div className={this.state.loginStyle}>
-          <div className="col-sm-6">
-            <h3 id="highlight" className="heroTitle">Khet Se Ghar Tak</h3>
-            <div id="info">
-              <p className="heroText">Providing you fresh, and organic vegetables.. Straight from the farm!</p>
-              <button className="customBtn"><Link className="customBtn" to="/login">Login</Link></button>
-              <br />
-              <br />
-              <button className="customBtnSecondary"><Link className="customBtnSecondaryx" to="/register">Register</Link></button>
+    let displayContent = null;
+    if(this.state.isLoading) {
+      displayContent = (
+        <div className="text-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <h3>Please wait while the mart opens up for you!</h3>
+        </div>
+      )
+    } else {
+      displayContent = (
+        <div>
+          <div className={this.state.loginStyle}>
+            <div className="col-sm-6">
+              <h3 id="highlight" className="heroTitle">Khet Se Ghar Tak</h3>
+              <div id="info">
+                <p className="heroText">Providing you fresh, and organic vegetables.. Straight from the farm!</p>
+                <button className="customBtn"><Link className="customBtn" to="/login">Login</Link></button>
+                <br />
+                <br />
+                <button className="customBtnSecondary"><Link className="customBtnSecondaryx" to="/register">Register</Link></button>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="heroImg" src="./images/farmerlandingpage.svg" />
             </div>
           </div>
-          <div className="col-sm-6">
-            <img className="heroImg" src="./images/farmerlandingpage.svg" />
-          </div>
-        </div>
-        <div className={this.state.logoutStyle}>
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search for veggies.." aria-label="Search for veggies.." aria-describedby="button-addon2" onChange={this.onSearchQueryChange} value={this.state.searchQuery} />
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.productSearch}>Search</button>
-          </div>
-          <div className={this.state.queryNull}>
-            <div className="col-md-3"></div>
-            <div className="col-md-6">
-              <h4 className="content">Oops!<br />No results found..</h4>
-              <img className="heroImg" src="./images/searchquerynull.svg" />
+          <div className={this.state.logoutStyle}>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Search for veggies.." aria-label="Search for veggies.." aria-describedby="button-addon2" onChange={this.onSearchQueryChange} value={this.state.searchQuery} />
+              <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.productSearch}>Search</button>
             </div>
-            <div className="col-md-3"></div>
-          </div>
-          <div>
-            {
-              this.state.products.map((product, pos) => {
-                return(
-                  <ProductItem product={product} key={pos} />
-                )
-              })
-            }
+            <div className={this.state.queryNull}>
+              <div className="col-md-3"></div>
+              <div className="col-md-6">
+                <h4 className="content">Oops!<br />No results found..</h4>
+                <img className="heroImg" src="./images/searchquerynull.svg" />
+              </div>
+              <div className="col-md-3"></div>
+            </div>
+            <div>
+              {
+                this.state.products.map((product, pos) => {
+                  return(
+                    <ProductItem product={product} key={pos} />
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
-      </div>
-    );
+      )
+    }
+    return displayContent;
   }
 
   productSearch = () => {
