18693f14281c50fa27dbd14b645f5fbf8fc59396
visshhhal <vludrani@gmail.com>
2021-03-29 09:09:29 +0530

    shifted to firebase realtime database

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,31 +3,41 @@ import firebase from 'firebase';
 
 class ProductDetails extends React.Component{
   state = {
-    buyer: null,
+    buyerEmail: '',
+    buyerName: '',
+    buyingStatus: '',
     price: null,
-    farmer: null,
-    crop: null,
+    farmerName: '',
+    farmerEmail: '',
+    crop: '',
     quantity_kg: null,
-    cropCategory: null
+    cropCategory: ''
   }
 
   componentDidMount() {
     let productId = this.props.match.params.id;
-    firebase.firestore().collection("product").doc(`${productId}`).onSnapshot((doc) => {
+    console.log(productId);
+    firebase.database().ref("product/" + productId).on('value', (doc) => {
+      console.log(doc.val());
       this.setState({
-        price: `${doc.data().price}/Kg`,
-        farmer: doc.data().farmer,
-        crop: doc.data().crop,
-        quantity_kg: doc.data().quantity_kg
+        price: `${doc.val().price}/Kg`,
+        farmerName: doc.val().farmerName,
+        farmerEmail: doc.val().farmerEmail,
+        crop: doc.val().crop,
+        quantity_kg: doc.val().quantity_kg
       });
       document.title = `${this.state.crop} | Krishi Mart`;
-      if(doc.data().quantity_kg <= 10) {
+      if(doc.val().quantity_kg <= 10) {
         this.setState({cropCategory: 'Price fixed at'});
       } else {
         this.setState({cropCategory: 'Current bid at'})
       }
-      if(doc.data().buyer) {
-        this.setState({buyer: `Current bid by ${doc.data().buyer}`})
+      if(doc.val().buyerName) {
+        this.setState({
+          buyingStatus: `Current bid by ${doc.val().buyerName}`,
+          buyerEmail: doc.val().buyerEmail,
+          buyerName: doc.val().buyerName
+        })
       }
     });
   }
@@ -38,9 +48,9 @@ class ProductDetails extends React.Component{
         <img className="col-md-6" src="https://images.pexels.com/photos/1300972/pexels-photo-1300972.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
         <div className="col-md-6">
           <h2>{this.state.crop}</h2>
-          <p>Sold by {this.state.farmer}</p>
+          <p>Sold by {this.state.farmerName}</p>
           <p>{this.state.quantity_kg} Kg - {this.state.cropCategory} - Rs {this.state.price}</p>
-          <p>{this.state.buyer}</p>
+          <p>{this.state.buyingStatus}</p>
         </div>
       </div>
     );
