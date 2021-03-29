0ad9762009e4c10350a52731a8eaf69fb2ce4931
visshhhal <vludrani@gmail.com>
2021-03-29 18:52:55 +0530

    added farmer-side UIon farmer login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import ProductItem from './ProductItem';
 class Home extends React.Component {
   state = {
     loggedInUser: '',
+    loggedInUserCategory: '',
     loginStyle: '',
     logoutStyle: '',
     products: [],
@@ -30,6 +31,24 @@ class Home extends React.Component {
           logoutStyle: "display-block row",
           isLoading: false
         });
+        // check if the user is farmer, and set the screen content accordingly
+        firebase.database().ref('user/').on('value', (snapshot) => {
+          let userdb = [];
+          snapshot.forEach((doc) => {
+            userdb = userdb.concat(doc.val());
+          })
+          for(let u of userdb) {
+            if(u.category === 'farmer') {
+              this.setState({
+                loggedInUserCategory: 'farmer'
+              });
+            } else {
+              this.setState({
+                loggedInUserCategory: 'consumer'
+              });
+            }
+          }
+        });
       } else { // user is signed out
         // display the webpage for the user to register/login
         this.setState({
@@ -39,30 +58,45 @@ class Home extends React.Component {
         });
       }
     });
-    if(this.state.searchQuery) {
-      this.setState({
-        searchProgress: (
-          <div className="text-center">
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
+    if(this.state.loggedInUserCategory === 'consumer') {
+      if(this.state.searchQuery) {
+        this.setState({
+          searchProgress: (
+            <div className="text-center">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+              <h3>Let us check the stockroom..</h3>
             </div>
-            <h3>Let us check the stockroom..</h3>
-          </div>
-        )
-      });
+          )
+        });
+      } else {
+        this.setState({
+          searchProgress: (
+            <div className="row">
+              <div className="col-sm-3"></div>
+              <div className="col-sm-6">
+                <h3>Hey there!<br />What are you looking for..?<br />Please let us know via that search bar..</h3>
+                <img className="heroImg" src="./images/searchquerynull.svg" alt="Delivery boy here to deliver your order." />
+              </div>
+              <div className="col-sm-3"></div>
+            </div>
+          )
+        });      
+      }
     } else {
       this.setState({
         searchProgress: (
           <div className="row">
             <div className="col-sm-3"></div>
             <div className="col-sm-6">
-              <h3>Hey there!<br />What are you looking for..?<br />Please let us know via that search bar..</h3>
-              <img className="heroImg" src="./images/searchquerynull.svg" alt="Delivery boy here to deliver your order." />
+              <h3>Welcome back to Krishi Mart!<br />Your products appear here!</h3>
+              <img style={{width: '75%', height: 'auto'}} src="./images/farmer-crop-upload.svg" alt="Delivery boy here to deliver your order." />
             </div>
             <div className="col-sm-3"></div>
           </div>
         )
-      });      
+      });  
     }
   }
 
@@ -78,45 +112,72 @@ class Home extends React.Component {
         </div>
       )
     } else {
-      displayContent = (
-        <div>
-          <div className={this.state.loginStyle}>
-            <div className={this.state.heroContentStyle}>
-              <h3 id="highlight" className="heroTitle">Khet Se Ghar Tak</h3>
-              <div id="info">
-                <p className="heroText">Providing you fresh, and organic vegetables.. Straight from the farm!</p>
-                <button className="customBtn"><Link className="customBtn" to="/login">Login</Link></button>
-                <br />
-                <br />
-                <button className="customBtnSecondary"><Link className="customBtnSecondaryx" to="/register">Register</Link></button>
+      if(this.state.loggedInUserCategory === 'consumer') {
+        displayContent = (
+          <div>
+            <div className={this.state.loginStyle}>
+              <div className={this.state.heroContentStyle}>
+                <h3 id="highlight" className="heroTitle">Khet Se Ghar Tak</h3>
+                <div id="info">
+                  <p className="heroText">Providing you fresh, and organic vegetables.. Straight from the farm!</p>
+                  <button className="customBtn"><Link className="customBtn" to="/login">Login</Link></button>
+                  <br />
+                  <br />
+                  <button className="customBtnSecondary"><Link className="customBtnSecondaryx" to="/register">Register</Link></button>
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <img className="heroImg" src="./images/farmerlandingpage.svg" alt="Farmer with a hen in his hand, smiling." />
               </div>
             </div>
-            <div className="col-sm-6">
-              <img className="heroImg" src="./images/farmerlandingpage.svg" alt="Farmer with a hen in his hand, smiling." />
+            <div className={this.state.logoutStyle}>
+              <div id="info" className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Search for veggies.." aria-label="Search for veggies.." aria-describedby="button-addon2" onChange={this.onSearchQueryChange} value={this.state.searchQuery} />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.productSearch}>Search</button>
+              </div>
+              <div id="highlight" className="content container">
+                {this.state.searchProgress}
+              </div>
+              <div id="info">
+                <h3 id="highlight">Results for '{this.state.searchQuery}'</h3>
+                <br />
+                {
+                  this.state.products.map((product, pos) => {
+                    return(
+                      <ProductItem product={product} key={pos} />
+                    )
+                  })
+                }
+              </div>
             </div>
           </div>
-          <div className={this.state.logoutStyle}>
-            <div id="info" className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="Search for veggies.." aria-label="Search for veggies.." aria-describedby="button-addon2" onChange={this.onSearchQueryChange} value={this.state.searchQuery} />
-              <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.productSearch}>Search</button>
+        )
+      } else {
+        displayContent = (
+          <div>
+            <div className={this.state.loginStyle}>
+              <div className={this.state.heroContentStyle}>
+                <h3 id="highlight" className="heroTitle">Khet Se Ghar Tak</h3>
+                <div id="info">
+                  <p className="heroText">Providing you fresh, and organic vegetables.. Straight from the farm!</p>
+                  <button className="customBtn"><Link className="customBtn" to="/login">Login</Link></button>
+                  <br />
+                  <br />
+                  <button className="customBtnSecondary"><Link className="customBtnSecondaryx" to="/register">Register</Link></button>
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <img className="heroImg" src="./images/farmerlandingpage.svg" alt="Farmer with a hen in his hand, smiling." />
+              </div>
             </div>
-            <div id="highlight" className="content container">
-              {this.state.searchProgress}
-            </div>
-            <div id="info">
-              <h3 id="highlight">Results for '{this.state.searchQuery}'</h3>
-              <br />
-              {
-                this.state.products.map((product, pos) => {
-                  return(
-                    <ProductItem product={product} key={pos} />
-                  )
-                })
-              }
+            <div className={this.state.logoutStyle}>
+              <div id="highlight" className="content container">
+                {this.state.searchProgress}
+              </div>
             </div>
           </div>
-        </div>
-      )
+        )
+      }
     }
     return displayContent;
   }
@@ -137,7 +198,7 @@ class Home extends React.Component {
 
   productSearch = () => {
     let resultKey = [];
-    let previousProductsData;
+    let previousProductsData = '';
     while(this.state.products.length) {
       previousProductsData = this.state.products.pop();
     }
@@ -145,7 +206,6 @@ class Home extends React.Component {
       querySnapshot.forEach((doc) => {
         // collect all the data in a variable
         resultKey = resultKey.concat(doc);
-        console.log(resultKey);
       });
       // loop through the results to check for matching results
       let queryData = []; // variable to store the matching results
