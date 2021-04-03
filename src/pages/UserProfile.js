42c43a31cf770899a3718966e96f27ff65707104
visshhhal <vludrani@gmail.com>
2021-04-03 20:34:11 +0530

    added user profile page with purchase history

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -9,16 +9,47 @@ class UserProfile extends React.Component {
 		category: '',
 		phNo: null,
 		address: '',
-		dataStatus: 'Loading...'
+		dataStatus: 'Loading...',
+		pageStyle: 'row',
+		products: []
 	}
 
 	componentDidMount() {
+		document.title = `${this.state.email} | Krishi Mart`;
+		var mql = window.matchMedia("screen and (max-width: 576px)");
+    this.pageStyle(mql)
+    mql.addEventListener('change', this.pageStyle);
 		firebase.auth().onAuthStateChanged((user) => {
 			if(user) {
 				this.setState({
 					userName: user.displayName,
 					email: user.email,
 					dataStatus: ''
+				})
+				firebase.database().ref('user/').on('value', (snapshot) => {
+					let loggedInUser;
+					let userdb = [];
+          snapshot.forEach((doc) => {
+            userdb = userdb.concat(doc.val());
+          })
+					for(let u of userdb) {
+						if(u.email === user.email) {
+							loggedInUser = u;
+						}
+					}
+					this.setState({
+						phNo: loggedInUser.phNo,
+						address: loggedInUser.address
+					})
+				})
+				firebase.database().ref('product/').on('value', (snapshot) => {
+					let product = [];
+					snapshot.forEach((doc) => {
+						if(doc.val().buyerEmail === this.state.email) {
+							product = product.concat(doc.val());
+						}
+					});
+					this.setState({products: product});
 				})
 			} else {
 				this.setState({
@@ -31,16 +62,45 @@ class UserProfile extends React.Component {
 	render() {
 		return(
 			<div>
-				<div id="product-details" className="row">
+				<div id="product-details" className={this.state.pageStyle}>
 					<img className="col-sm-4" src="https://www.flaticon.com/svg/vstatic/svg/848/848006.svg?token=exp=1616830677~hmac=c72242cc9aab76b7b770e10533c1f462" width="128" height="128" />
-					<div className="col-sm-8">
-						<p>
-							{this.state.userName}
-						</p>
+					<div className="col-sm-8" style={{padding: '10px'}}>
+						<p>{this.state.userName}</p>
+						<p><i class="fas fa-phone-alt"></i> {this.state.phNo}</p>
+						<p><i class="fas fa-at"></i> {this.state.email}</p>
+						<button className="customBtn" style={{borderRadius: '10px'}}><i className="fas fa-pen"></i> Edit</button>
+					</div>
+				</div>
+				<br />
+				<div className={this.state.pageStyle}>
+					<h4 style={{textAlign: 'center'}}>Your purchase history</h4>
+					<hr />
+					<br />
+					<div>
+						{
+							this.state.products.map((product, pos) => {
+								return(
+									<div>
+										<p>Crop: {product.crop}</p>
+										<p>Quantity: {product.quantity_kg} Kg</p>
+										<p>Price: {product.crop}/Kg</p>
+										<p>Sold by: {product.farmerName} ({product.farmerEmail})</p>
+									</div>
+								)
+							})
+						}
 					</div>
 				</div>
 			</div>
 		)
+	}
+
+	pageStyle = (e) => {
+		if(e.matches) {
+			this.setState({pageStyle: 'row content'});
+		} else {
+			this.setState({pageStyle: 'row'});
+		}
 	}
 
 }
