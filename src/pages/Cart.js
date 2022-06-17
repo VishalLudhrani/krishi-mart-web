dfbf3700ebb7059530ff8dfa0a381049af802c97
Vishal Ludhrani <vludrani@gmail.com>
2022-06-17 12:20:31 +0530

    chore: extract common components to make the code modular

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import CartItemsList from "components/elements/Cart/CartItemsList";
+import { useDispatch, useSelector } from "react-redux";
 import withAuth from "guards/with-auth";
+import List from "components/common/List/List";
+import { useHistory, useLocation } from "react-router-dom";
+import { cartActions } from "store/cart-slice";
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart.data);
+  const location = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   return (
     <section className="mt-4">
@@ -14,7 +19,52 @@ const Cart = () => {
         </h1>
         <p style={{ fontSize: "1.25rem" }}>Cart total: <strong>&#8377;{cart.totalAmount}</strong></p>
       </div>
-      <CartItemsList />
+      {cart.totalItems > 0 ? (
+        <React.Fragment>
+          <ul className="list-group">
+            {cart.items.map(item => (
+              <List
+                key={item.id}
+                primary={item.cropName}
+                secondary={
+                  <React.Fragment>
+                    <p className="mb-1">
+                      <span className="fw-bold">Sold by:</span> {item.farmerName}
+                    </p>
+                    <p className="mb-1">
+                      <span className="fw-bold">Price:</span> &#8377;{item.price}/kg
+                    </p>
+                    <p className="mb-1">
+                      <span className="fw-bold">Quantity:</span> {item.quantity} kg
+                    </p>
+                  </React.Fragment>
+                }
+                actions={
+                  <React.Fragment>
+                    <p className="mb-1">
+                      <span className="fw-bold">Total amount:</span> &#8377;
+                      {item.quantity * item.price}
+                    </p>
+                    <button className="btn btn-link link-danger" onClick={() => {dispatch(cartActions.removeFromCart({ id: item.id }))}}>Remove from cart</button>
+                  </React.Fragment>
+                }
+              />
+            ))}
+          </ul>
+          {location.pathname !== "/checkout" && (
+            <button
+              className="btn btn-success mt-4"
+              onClick={() => {
+                history.push("/checkout");
+              }}
+            >
+              Checkout
+            </button>
+          )}
+        </React.Fragment>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
     </section>
   );
 };
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,15 +1,17 @@
-import CartItemsList from "components/elements/Cart/CartItemsList";
+import React from "react";
 import useUser from "hooks/useUser";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { cartActions } from "store/cart-slice";
 import { historyActions } from "store/history-slice";
+import List from "components/common/List/List";
 
 const Checkout = () => {
   const { data: user } = useUser();
   const cart = useSelector(state => state.cart.data);
   const dispatch = useDispatch();
   const history = useHistory();
+  const location = useLocation();
 
   const handleCheckout = () => {
     dispatch(historyActions.addToHistory({ items: cart.items }));
@@ -21,7 +23,52 @@ const Checkout = () => {
     <>
       <strong><h1 className="mt-4" id="highlight">Checkout</h1></strong>
       <p>Delivering to {user.name.split(" ")[0]} at {user.address}</p>
-      <CartItemsList />
+      {cart.totalItems > 0 ? (
+        <React.Fragment>
+          <ul className="list-group">
+            {cart.items.map(item => (
+              <List
+                key={item.id}
+                primary={item.cropName}
+                secondary={
+                  <React.Fragment>
+                    <p className="mb-1">
+                      <span className="fw-bold">Sold by:</span> {item.farmerName}
+                    </p>
+                    <p className="mb-1">
+                      <span className="fw-bold">Price:</span> &#8377;{item.price}/kg
+                    </p>
+                    <p className="mb-1">
+                      <span className="fw-bold">Quantity:</span> {item.quantity} kg
+                    </p>
+                  </React.Fragment>
+                }
+                actions={
+                  <React.Fragment>
+                    <p className="mb-1">
+                      <span className="fw-bold">Total amount:</span> &#8377;
+                      {item.quantity * item.price}
+                    </p>
+                    <button className="btn btn-link link-danger" onClick={() => {dispatch(cartActions.removeFromCart({ id: item.id }))}}>Remove from cart</button>
+                  </React.Fragment>
+                }
+              />
+            ))}
+          </ul>
+          {location.pathname !== "/checkout" && (
+            <button
+              className="btn btn-success mt-4"
+              onClick={() => {
+                history.push("/checkout");
+              }}
+            >
+              Checkout
+            </button>
+          )}
+        </React.Fragment>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
       <button className="btn btn-success mt-4 btn-lg" onClick={handleCheckout}>{`Pay ₹${cart.totalAmount} and checkout`}</button>
     </>
   )
